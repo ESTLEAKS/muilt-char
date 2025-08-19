f203c85318d29be29375080e3dee14e94fa791da
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-18 20:21:29 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Carson'
+description 'Multi Character UI using UMS'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
+
+ui_page 'html/index.html'
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
