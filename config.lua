2ae421f934144ce89ec08e8f26936e1218dde0b5
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-18 20:22:21 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.MaxCharacters = 3 -- Number of character slots
+Config.DefaultTemplate = {
+    firstname = "",
+    lastname = "",
+    dob = "",
+    gender = "",
+    job = "unemployed"
+}
